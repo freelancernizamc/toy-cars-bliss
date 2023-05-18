ec5e09fe7c5e3f5c6267ae823d782865797917de
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-18 10:27:14 +0600

    all toys component created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../components/Home/Home/Home";
+import AllToys from "../components/AllToys/AllToys";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
                 path: '/',
                 element: <Home />
             },
+            {
+                path: '/alltoys',
+                element: <AllToys />
+            }
         ]
     },
 ]);
---
+++ src/components/AllToys/AllToys.jsx
@@ -0,0 +1,11 @@
+
+
+const AllToys = () => {
+    return (
+        <div>
+            <h2>All Toy Here</h2>
+        </div>
+    );
+};
+
+export default AllToys;
