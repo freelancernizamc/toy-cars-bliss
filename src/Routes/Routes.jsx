510040922269e0cb775feb87393fc264793a4085
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-18 09:26:19 +0600

    React router setup completed

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../components/Home/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+        ]
+    },
+]);
+
+
+
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes.jsx';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
